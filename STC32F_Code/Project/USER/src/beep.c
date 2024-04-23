da1b09ebfad72691a4caf52dd2bfb0d6b07c2e2c
Alaye-Dong <AlayeVast@outlook.com>
2024-04-23 19:39:30 +0800

    build：STC32F

---
+++ STC32F_Code/Project/USER/src/beep.c
@@ -0,0 +1,15 @@
+#include "headfile.h"
+
+#define BEEP P32
+
+void BEEP_Init(void)
+{
+    gpio_mode(P3_2, GPO_PP); // ·äÃùÆ÷
+}
+
+void BEEP_ON_ms(int ms)
+{
+    BEEP = 1;
+    delay_ms(ms);
+    BEEP = 0;
+}
